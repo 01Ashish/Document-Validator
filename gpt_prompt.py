a6e38b15ec7813d262e7a93aa2b6c7e987f39104
Ashish Dixit <97104619+01Ashish@users.noreply.github.com>
2025-01-06 23:31:42 +0530

    Update gpt_prompt.py

--- gpt_prompt.py
+++ gpt_prompt.py
@@ -4,7 +4,7 @@ import requests
 import os
 from prompts_to_json import regex_search,convert_keys_to_camel_case,rename_degree_key
 from dotenv import load_dotenv
-from prompt import board_certificate_prompt, dea_prompt, pli_prompt, ofac_prompt, sam_prompt, medicare_opt_out_prompt, oig_prompt, npi_prompt, caqh_prompt 
+from prompt import board_certificate_prompt, dea_prompt, pli_prompt, ofac_prompt, sam_prompt, medicare_opt_out_prompt, oig_prompt, npi_prompt, caqh_prompt, other_prompt 
 
 
 load_dotenv()
@@ -31,6 +31,8 @@ def process_image_first(image_path,user_input):
         prompt = oig_prompt
     elif user_input["document_name"] == "board_certificate":
         prompt = board_certificate_prompt
+    else:
+        prompt = other_prompt
     
     with open(image_path, "rb") as image:
         base64_image = base64.b64encode(image.read()).decode("utf-8")
